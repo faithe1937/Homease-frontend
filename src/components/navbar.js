9aa1a98bf4f6ea7ce302f881ee84ff818aa5d70b
Faith <faithe1937@gmail.com>
2020-02-08 19:44:01 -0600

    custom hook works for forms

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,39 +1,61 @@
-import React from "react";
-import { useUserSession } from "../context/user-context";
+import React, { Component } from "react";
+import { Input, Menu } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
-const ProfileNav = () => {
-  const { user, logOut } = useUserSession();
+class Navbar extends Component {
+  state = { activeItem: "home" };
 
-  function onClick(e) {
-    e.preventDefault();
-    logOut();
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
+  render() {
+    const { activeItem } = this.state;
+
+    return (
+      <div>
+        <Menu pointing>
+          <Menu.Item
+            as={Link}
+            to="/"
+            name="home"
+            active={activeItem === "home"}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            as={Link}
+            to="/Profile"
+            name="Profile"
+            active={activeItem === "Profile"}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            as={Link}
+            to="/projectBoard"
+            name="ProjectBoard"
+            active={activeItem === "ProjectBoard"}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            as={Link}
+            to="/Messages"
+            name="Messages"
+            active={activeItem === "Messages"}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            as={Link}
+            to="/Photos"
+            name="Photos"
+            active={activeItem === "Photos"}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Menu position="right">
+            <Menu.Item>
+              <Input icon="search" placeholder="Search..." />
+            </Menu.Item>
+          </Menu.Menu>
+        </Menu>
+      </div>
+    );
   }
-
-  return (
-    <ul class="navbar-nav">
-      <li className="nav-item">
-        <span className="navbar-text"> Welcome {user.displayName}</span>
-      </li>
-      {!user._guest && (
-        <li className="nav-item">
-          <a className="nav-link" href="#" oncClick={onClick}>
-            log out
-          </a>
-        </li>
-      )}
-    </ul>
-  );
-};
-
-const NavBar = () => (
-  <div className="navba">
-    <div className="container">
-      <a className="navbar-brand" href="/">
-        NEW NEW
-      </a>
-      <ProfileNav />
-    </div>
-  </div>
-);
-
-export default NavBar;
+}
+export default Navbar;
